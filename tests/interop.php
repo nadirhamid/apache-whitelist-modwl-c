5ec5b586083c3230a2961d2873f61039530d2d58
U-Nadir-PC\Nadir <Nadir@Nadir-PC.(none)>
2014-08-01 15:32:26 -0600

    Added interop example

---
+++ tests/interop.php
@@ -0,0 +1,10 @@
+<?php
+/* An example for PHP use with mod_wl
+ * this will output all the modules details 
+ */
+
+echo "Original Address is: " . $_SERVER{'MODWL_ORIGINAL'} . "<br />";
+echo "Reverse DNS is: " . $_SERVER{'MODWL_REVERSE_DNS'} . "<br/>";
+echo "Forward DNS is: " . $_SERVER{'MODWL_FORWARD_DNS'} . "<br/>";
+echo "Status: " . $_SERVER{'MODWL_STATUS'};
+?>
